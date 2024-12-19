0a53236e48bf13c6a90d1372653b9f43dfa7bab2
tharushadinuth(pro) <tharushadinuth21@gmail.com>
2024-12-20 01:07:17 +0530

    new

---
+++ server/models/Service.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const serviceSchema = mongoose.Schema({
+  service: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String, // Base64 encoded image
+    required: true,
+  },
+});
+
+const Service = mongoose.model("Service", serviceSchema);
+
+module.exports = Service;
